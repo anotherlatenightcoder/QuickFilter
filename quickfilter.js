ef84033ffa83871767a4869da4d7b0f3188f45c4
Morne Gouws <webejame@gmail.com>
2012-10-24 21:16:40 +0200

    Added quickfilter.js

---
+++ quickfilter.js
@@ -0,0 +1,25 @@
+jQuery('input#quickfilter').bind('keyup', function(){
+   var searchlen = (jQuery(this).val()).length;
+   var searchstr = jQuery(this).val();
+   jQuery.each(jQuery('table#filter tbody tr'), function(){
+       jQuery(this).css('display','table-row');
+   }); 
+   jQuery.each(jQuery('table#filter tbody tr td#col'), function(){
+       var inputvalue = jQuery(this).text().toUpperCase();
+       var pos = (inputvalue).indexOf(searchstr.toUpperCase());              
+       if (pos != "-1") { 
+            var mainString = jQuery(this).text(); 
+            var arr = mainString.substring(pos,searchlen); 
+            var len = arr.length;
+            var finalString="";
+            if (pos != 0) {
+                finalString = mainString.substring(0, pos);
+            }
+            finalString += '<span class="srchslctn">'+searchstr.toUpperCase()+'</span>';
+            finalString += mainString.substring(pos+searchlen, mainString.length);
+            jQuery(this).html(finalString);
+       } else {
+           jQuery(this).parents('table#filter tbody tr:first').css('display','none');
+       }
+   });
+});
