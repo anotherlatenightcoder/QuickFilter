05884651048384030018f79c54f5183e862ef3b1
Morne Gouws <webejame@gmail.com>
2012-10-26 10:55:03 +0200

    Issue #3 turned into a function, pass through params

--- quickfilter.js
+++ quickfilter.js
@@ -1,27 +1,28 @@
-jQuery(document).ready(function(){
-    jQuery('input#quickfilter').bind('keyup', function(){
-       var searchlen = (jQuery(this).val()).length;
-       var searchstr = (jQuery(this).val()).toUpperCase();
-       jQuery.each(jQuery('table#filter tbody tr'), function(){
-           jQuery(this).css('display','table-row');
-       }); 
-       jQuery.each(jQuery('table#filter tbody tr td:first-child'), function(){
-           var inputvalue = jQuery(this).text().toUpperCase();
-           var pos = (inputvalue).indexOf(searchstr.toUpperCase());              
-           if (pos != "-1") {
-                var mainString = jQuery(this).text(); 
-                var arr = mainString.substring(pos,searchlen); 
-                var len = arr.length;
-                var finalString="";
-                if (pos != 0) {
-                    finalString = mainString.substring(0, pos);
-                }
-                finalString += '<span style="background-color: yellow;">'+searchstr.toUpperCase()+'</span>';
-                finalString += mainString.substring(pos+searchlen, mainString.length);
-                jQuery(this).html(finalString);
-           } else {
-               jQuery(this).parents('table#filter tbody tr:first').css('display','none');
-           }
-       });
+function quickFilter(inputid, tableid, child) {
+    child = typeof child !== 'undefined' ? child : 1;
+    $('#'+inputid).bind('keyup', function(){
+    var searchlen = ($(this).val()).length;
+    var searchstr = ($(this).val()).toUpperCase();
+    $.each($('table#'+tableid+' tbody tr'), function(){
+       $(this).css('display','table-row');
+    }); 
+    $.each($('table#'+tableid+' tbody tr td:nth-child('+child+')'), function(){
+       var inputvalue = $(this).text().toUpperCase();
+       var pos = (inputvalue).indexOf(searchstr.toUpperCase());              
+       if (pos != "-1") {
+            var mainString = $(this).text(); 
+            var arr = mainString.substring(pos,searchlen); 
+            var len = arr.length;
+            var finalString="";
+            if (pos != 0) {
+                finalString = mainString.substring(0, pos);
+            }
+            finalString += '<span style="background-color: yellow;">'+searchstr.toUpperCase()+'</span>';
+            finalString += mainString.substring(pos+searchlen, mainString.length);
+            $(this).html(finalString);
+       } else {
+           $(this).parents('table#'+tableid+' tbody tr:first').css('display','none');
+       }
     });
-});
+    });
+}
