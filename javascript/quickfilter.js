6ec543f8afedd084141c366bda1333fa05094fd0
Morne Gouws <webejame@gmail.com>
2012-10-26 17:22:08 +0200

    Issue #5 no results message

--- javascript/quickfilter.js
+++ javascript/quickfilter.js
@@ -1,28 +1,34 @@
 function quickFilter(inputid, tableid, child) {
     child = typeof child !== 'undefined' ? child : 1;
     $('#'+inputid).bind('keyup', function(){
-    var searchlen = ($(this).val()).length;
-    var searchstr = ($(this).val()).toUpperCase();
-    $.each($('table#'+tableid+' tbody tr'), function(){
-       $(this).css('display','table-row');
-    }); 
-    $.each($('table#'+tableid+' tbody tr td:nth-child('+child+')'), function(){
-       var inputvalue = $(this).text().toUpperCase();
-       var pos = (inputvalue).indexOf(searchstr.toUpperCase());              
-       if (pos != "-1") {
-            var mainString = $(this).text(); 
-            var arr = mainString.substring(pos,searchlen); 
-            var len = arr.length;
-            var finalString="";
-            if (pos != 0) {
-                finalString = mainString.substring(0, pos);
+        var searchlen = ($(this).val()).length;
+        var searchstr = ($(this).val()).toUpperCase();
+        $.each($('table#'+tableid+' tbody tr'), function(){
+           $(this).css('display','table-row');
+        }); 
+        $.each($('table#'+tableid+' tbody tr td:nth-child('+child+')'), function(){
+            var inputvalue = $(this).text().toUpperCase();
+            var pos = (inputvalue).indexOf(searchstr.toUpperCase());              
+            if (pos != "-1") {
+                var mainString = $(this).text(); 
+                var arr = mainString.substring(pos,searchlen); 
+                var len = arr.length;
+                var finalString="";
+                if (pos != 0) {
+                    finalString = mainString.substring(0, pos);
+                }
+                finalString += '<span style="background-color: yellow;">'+searchstr.toUpperCase()+'</span>';
+                finalString += mainString.substring(pos+searchlen, mainString.length);
+                $(this).html(finalString);
+            } else {
+                $(this).parents('table#'+tableid+' tbody tr:first').css('display','none');
             }
-            finalString += '<span style="background-color: yellow;">'+searchstr.toUpperCase()+'</span>';
-            finalString += mainString.substring(pos+searchlen, mainString.length);
-            $(this).html(finalString);
-       } else {
-           $(this).parents('table#'+tableid+' tbody tr:first').css('display','none');
-       }
-    });
+        });
+        var count = $('table#'+tableid+' tbody tr:visible').length;
+        if (count == 0) {
+            $('table#'+tableid).append("<div id=\"noresults\">There are no Results</div>");
+        } else {
+            $("#noresults").remove();
+        }
     });
 }
